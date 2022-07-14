53d617053adf2b0a97eb0a737ab194b4b3da6a46
Ramonie <55808322+Ramonie@users.noreply.github.com>
2022-07-14 16:02:39 -0300

    Update prova 3 algoritmos.py

--- prova 3 algoritmos.py
+++ prova 3 algoritmos.py
@@ -1,9 +1,10 @@
 def ini_matriz(tamanho):
-    #iniciar a matriz
-    m = [[" " for g in range(tamanho[1])]for i in range(tamanho[0])]
+# Para iniciar a matriz:
+    m = [['' for g in range (tamanho[1])] for i in range(tamanho[0])]
     return m
 
 def mostra_matriz(matriz):
+# Para printar a matriz:
     print('  1   2   3   4   5   6')
     print("-------------------------")
     for linha in matriz:
@@ -12,100 +13,221 @@ def mostra_matriz(matriz):
             print(elemento, end=" | ")
         print("")
     print("-------------------------")
-
-def jogada(matriz,coluna):
-    #colocando o jogo na matriz
-    for i in range(len(matriz)):
-        for j in range(len(matriz[i])):
-             matriz[i][coluna] = matriz[j][coluna]
-        return matriz
 def trocar_jogador(jogador_atual):
-    if jogador_atual == 'X':
-        return 'O'
-    else:
+    
+    if (jogador_atual%2+1 == 1 ):
         return 'X'
+    else:
+        return 'O'
 
 
+def jogada(matriz,coluna,token):
 
+  # Colocar o jogo matriz
+
+
+  for i in range(len(matriz)-1,-1,-1):
+
+     token , matriz[i][coluna] = matriz[i][coluna], token
+
+
+  return matriz
 
 def verificar(jogo):
-    #verificar jogadores
-    for i in  range(len(jogo)):
-        cont = trocar_jogador#vai contar a quantidade de peças na horizontal da matriz
-        for j in range(len(jogo[i])):
-            if cont[1]>=4:
-                return cont[0]
-            elif jogo[i][j]:
-                if cont[0] == jogo[i][j]:
-                    cont[1]+=1
-                else:
-                    cont[0]= jogo[i][j]
-                    cont[1]=1
-            else:
-                cont = [0,0]
-        for i in range(len(jogo[1])):
-            #matriz contadora de peças verticais
-           
-            for j in range(len(jogo)):
-                if jogo[j][i]:
-                 if cont[0]==jogo[j][i]:
+
+  # Verificação de jogadores
+
+  for i in range(len(jogo)):
+
+    cont = [0,0] #Matriz para contar quantas pecas tem na horizontal
+
+
+    for j in range(len(jogo[i])):
+
+        if cont[1] >= 4:
+
+              return cont[0]
+        elif jogo[i][j]:
+
+              if cont[0] ==jogo[i][j]:
+
+                  cont[1]+=1
+
+              else:
+
+                  cont[0] = jogo[i][j]
+
+                  cont[1] = 1
+
+        else:
+
+              cont = [0,0]
+
+    for i in range(len(jogo[1])):
+
+      cont = [0,0]
+
+#Matriz contador de pencas verticais
+
+      for j in range(len(jogo)):
+
+        if jogo[j][i]:
+
+
+              if cont[0] ==jogo[j][i]:
+
+                  cont[1]+=1
+
+              else:
+
+
+                  cont[0] = jogo[j][i]
+
+
+                  cont[1] = 1
+
+
+        else:
+
+
+              cont = [0,0]
+
+
+        if cont[1] >= 4:
+
+
+              return cont[0]
+
+               
+
+  for i in range(4-1,len(jogo)):
+
+      for g in range(len(jogo[1])-(4-1)):
+
+          cont = [0,0]
+
+# matriz para contar quantas pecas tem na diagonal
+
+          for k in range(4):
+
+              if jogo[i-k][g+k]:
+
+
+                  if cont[0] ==jogo[i-k][g+k]:
+
+
+                      cont[1]+=1
+
+
+                  else:
+
+
+                      cont[0] = jogo[i-k][g+k]
+
+
+                      cont[1] = 1
+
+
+              else:
+
+
+                  break
+
+
+          if cont[1] >= 4:
+
+
+              return cont[0]
+
+  for i in range(len(jogo)-(4-1)):
+
+      for g in range(len(jogo[1])-(4-1)):
+
+          cont = [0,0]
+
+# Matriz contagem de diagonal
+
+          for k in range(4):
+
+              if jogo[i+k][g+k]:
+
+                  if cont[0] ==jogo[i+k][g+k]:
+
                      cont[1]+=1
-                 else:
-                    cont[0]= jogo[j][i]
-                    cont[1]=1
-            else:
-                cont=[0,0]
-                if cont[1]>=4:
-                    return cont[0]
-        for i in range(4-1,len(jogo)):
-            for g in range(len(jogo[1])-(4-1)):
-                cont = [0,0]
 
-        for k in range(4):
-            #conta quantas peças na diagonal
-            if jogo[i-k][g+k]:
-                if cont[0]==jogo[i-k][g+k]:
-                    cont[1]+=1
-                else:
-                    cont[0]= jogo[i-k][g+k]
-                    cont[1]=1
-            else:
-                break
-        if cont[1]>=4:
-            return cont[0]
-    for i in range(len(jogo)-(4-1)):
-        for g in range(len(jogo)-(4-1)):
-            cont = [0,0]
-            
-            for k in range(4):#contagem da matriz diagonal
-                if jogo[i+k][g+k]:
-                    if cont[0]==jogo[i+k][g+k]:
-                        cont[1]+=1
-                    else:
-                        break
-                if cont[1]>=4:
-                    return [0]
-                return 0
+                  else:
+
+                      cont[0] = jogo[i+k][g+k]
+
+                      cont[1] = 1
+
+
+              else:
+
+
+                  break
+
+
+          if cont[1] >= 4:
+
+
+              return cont[0]
+
+
+  return 0
+
+
+         
+
 def main():
-    tamanho = (6,6)#seta o tamanho do jogo
-    jogo = ini_matriz(tamanho)
-    ganhou = 0 #parar o jogo
-    jogador = 0 #quem vai ganhar
 
-    # while not ganhou:
-    #      mostra_matriz(jogo)
-    #      print("Quem vai jogar é o jogador:",jogador%2+1,".")
-    #      print("Escolha em qual coluna vai jogar(escolha de 1 a ,",tamanho[1],"):")
-    #      escolha = int(input())
-    #      while escolha<1 or escolha>tamanho[1]:
-    #          print("Número inválido")
-    #          print("Escolha em qual coluna vai jogar(escolha de 1 a ,",tamanho[1],"):")
-    #          escolha = int(input())
-    #      jogo = jogada(jogo,escolha+1,jogador%2+1)
-    ganhou = verificar(jogo)
-    #      jogador+=1
-    mostra_matriz(jogo)
-    print("O jogador",ganhou,"venceu!!!")
-    return 0
+  tamanho = (6,6) #serve pra setar o tamanho do jogo
 
-main()
+  jogo = ini_matriz(tamanho)
+
+  ganhou = 0
+
+# parar o jogo
+
+  jogador = 0
+  
+
+# quem vai jogar
+
+  while not ganhou:
+
+      mostra_matriz(jogo)
+
+      print('Quem vai jogar é o jogador ', trocar_jogador(jogador),'.')
+
+      print('Escolha em qual coluna você quer jogar (escolha de 1 a ',tamanho[1],'): ')
+
+
+
+      escolha = int(input())
+
+      while escolha<1 or escolha>tamanho[1]:
+
+              print('Número inválido.')
+
+              print('Escolha em qual coluna você quer jogar (escolha de 1 a ',tamanho[1],'): ')
+
+              escolha = int(input())
+
+      jogo = jogada(jogo,escolha-1, trocar_jogador(jogador))
+
+      ganhou = verificar(jogo)
+
+      jogador +=1
+
+             
+
+  mostra_matriz(jogo)
+
+  print('O jogador', ganhou,' venceu!!')
+
+  return main()
+
+if __name__ == "__main__":
+
+  main()
